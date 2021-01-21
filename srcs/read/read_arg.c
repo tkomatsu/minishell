8ad7cee7989fc311cf063d21c69dc6c6809b15b1
kefujiwa <31239797+kefujiwa@users.noreply.github.com>
2021-01-21 23:23:11 +0900

    Impl: _is_bad_quote function #6
    入力値にシングルクォート・ダブルクォートが不足しているか検知する関数を作成

--- srcs/read/read_arg.c
+++ srcs/read/read_arg.c
@@ -6,11 +6,35 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 22:34:03 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/01/18 22:45:02 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/01/21 23:17:16 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	_is_bad_quote(char *line)
+{
+	int	flag;
+
+	flag = 0;
+	if (*line == ''')
+		flag = QUOTE;
+	else if (*line == '"')
+		flag = DQUOTE;
+	line++;
+	while (*line)
+	{
+		if (!flag && *line == ''' && *(line - 1) != '\')
+			flag == QUOTE;
+		else if (!flag && *line == '"' && *(line - 1) != '\')
+			flag == DQUOTE;
+		else if ((flag == QUOTE && *line == ''') ||
+					(flag == DQUOTE && *line == '"'))
+			flag = 0;
+		line++;
+	}
+	return (flag);
+}
 
 int	read_arg(char **line)
 {
