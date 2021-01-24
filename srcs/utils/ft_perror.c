37dfab21523936e2277351291d985774e07d0138
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-01-24 22:52:35 +0900

    fix: modify error message
    定義外のエラーの時のメッセージを追加

--- srcs/utils/ft_perror.c
+++ srcs/utils/ft_perror.c
@@ -6,7 +6,7 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 23:36:39 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/01/24 18:08:50 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/01/24 22:51:25 by tkomatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,13 @@ static char	*ft_strerror(void)
 		return (NULL);
 	if (errno == 201)
 		ft_strlcpy(str, "command not found", 100);
-	else if (errno == 202)
-		ft_strlcpy(str, "ERROR!!!!!", 100);
+	else
+		ft_strlcpy(str, "undefine error", 100);
 	return (str);
 }
 
 void	ft_perror(char *s)
 {
-	extern int	errno;
-	int			i;
-
-	i = 0;
-	/*
-	while (s[i])
-	{
-		ft_putstr_fd(s[i], 2);
-		ft_putstr_fd(": ", 2);
-		i++;
-	}
-	*/
 	ft_putstr_fd(s, 2);
 	ft_putstr_fd(": ", 2);
 	if (200 <= errno && errno < 299)
