91f85aff8b7ddaf92d0cdf0e6ad253ed9f6ac536
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-02-04 22:17:46 +0900

    add: token list utilities
    トークンの双方向リストの関数を追加

---
+++ srcs/utils/token_manegement.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_manegement.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/04 22:16:31 by tkomatsu          #+#    #+#             */
+/*   Updated: 2021/02/04 22:17:35 by tkomatsu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "minishell.h"
+
+t_token	*dlistnew(char *src, int sep)
+{
+	t_token	*new;
+
+	new = ft_calloc(sizeof(t_token), 1);
+	new->word = src;
+	new->type = sep;
+	new->prev = NULL;
+	new->next = NULL;
+	return (new);
+}
+
+t_token *dlisthead(t_token *tokens)
+{
+	if (!tokens)
+		return (NULL);
+	if (!tokens->prev)
+		return (tokens);
+	return (dlisthead(tokens->prev));
+}
+
+t_token	*dlistlast(t_token *tokens)
+{
+	if (!tokens)
+		return (NULL);
+	if (!tokens->next)
+		return (tokens);
+	return (dlistlast(tokens->next));
+}
+
+void	dlist_add_back(t_token **list, t_token *new)
+{
+	t_token	*end;
+
+	if (!list || !new)
+		return ;
+	if (!(*list))
+		*list = new;
+	else
+	{
+		dlistlast(*list)->next = new;
+		new->prev = end;
+	}
+}
