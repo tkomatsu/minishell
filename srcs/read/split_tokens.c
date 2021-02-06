2bccdf1673a51b19f10248d49fe98ef7d2e2762a
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-02-06 13:36:56 +0900

    fix: t_token test
    引数の文字列をt_tokenに変換するだけの関数に修正。
    これをベースに機能を追加していきます。

--- srcs/read/split_tokens.c
+++ srcs/read/split_tokens.c
@@ -6,7 +6,7 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 17:30:29 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/02/04 22:17:04 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/02/06 13:34:26 by tkomatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,54 @@ static int	is_metachar(char c)
 	return (0);
 }
 
-int		get_token(char **dst, char *src, int *index);
+/*
+int	get_token(char **dst, char *src, int start)
+{
+	int		i;
+	int		len;
+
+	i = start;
+	while (src[i])
+	{
+		if (src[i] == '\'')
+			break;
+//			return (squote(src, index));
+		else if (src[i] == '\"')
+			break;
+//			return (dquote(src, index));
+		i++;
+	}
+	len = i - start;
+	if (!(*dst = ft_calloc(sizeof(char), (len + 1))))
+		return (-1);
+	ft_strlcpy(*dst, (src + i), len);
+	return (i);
+}
 
 t_token	*split_tokens(char *line)
 {
-	t_token *head;
+	t_token *tokens;
 	char	*str;
 	int		sep;
 	int		i;
 
 	i = 0;
+	tokens = NULL;
 	while (line[i])
 	{
-		sep = get_token(&str, line, &i);
-		dlist_add_back(&head, dlistnew(str, sep));
-		i++;
+		i = get_token(&str, line, i);
+		sep = is_metachar(line[i]);
+		dlist_add_back(&tokens, dlistnew(str, sep));
 	}
-	return (head);
+	return (dlisthead(tokens));
+}
+*/
+
+t_token	*split_tokens(char *line)
+{
+	t_token	*tokens;
+
+	tokens = NULL;
+	dlist_add_back(&tokens, dlistnew(line, is_metachar(' ')));
+	return (tokens);
 }
