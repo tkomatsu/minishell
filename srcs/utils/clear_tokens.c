82881986b5077e6975ca7ecb84671d74097bad77
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-02-09 02:47:10 +0900

    Modify: change del_token.c to clear_tokens.c and add clear_tokens function
    - del_token.cファイルを編集し、clear_tokens関数を新規実装

--- srcs/utils/clear_tokens.c
+++ srcs/utils/clear_tokens.c
@@ -6,16 +6,33 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 21:56:59 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/01/26 22:37:24 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/02/09 00:57:15 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	del_token(void *ptr)
+static void	del_token(t_token *token)
 {
-	t_token *token;
+	if (token)
+	{
+		free(token->word);
+		free(token);
+		token = NULL;
+	}
+}
 
-	token = (t_token*)ptr;
-	ft_free(token->word);
+void		clear_tokens(t_token **tokens)
+{
+	t_token	*now;
+	t_token	*next;
+
+	now = *tokens;
+	while (now)
+	{
+		next = now->next;
+		del_token(now);
+		now = next;
+	}
+	*tokens = NULL;
 }
