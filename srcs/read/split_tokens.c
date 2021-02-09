688b6874d6ced2c5630c7f9efc79db4c91bfe775
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-02-09 14:32:29 +0900

    Fix: Delete condition statement
    - クォートを全て渡す処理に変更

--- srcs/read/split_tokens.c
+++ srcs/read/split_tokens.c
@@ -6,7 +6,7 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 17:30:29 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/02/07 15:45:12 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/02/09 14:32:12 by tkomatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,8 +72,6 @@ t_token	*split_tokens(char *line)
 	while (line[i])
 	{
 		len = get_index(line, i);
-		if (line[i] == '\'' || line[i] == '\"')
-			i++;
 		dlist_add_back(&tokens, dlistnew(ft_substr(line, i, len), is_metachar(line[i + len])));
 		i = i + len + 1;
 	}
