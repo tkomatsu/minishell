9ee3e8da445f236e437d997be329b7605757c441
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-02-13 00:40:56 +0900

    fix(parse_tokens.c): fix bug when there are multiple continuous escape before quote or dquote
    - parse_word関数において、エスケープがエスケープされるケースに発生するバグを修正
    
    fix #6

--- srcs/parse/parse_tokens.c
+++ srcs/parse/parse_tokens.c
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 18:17:47 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/02/12 19:47:43 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/02/13 00:35:21 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,25 @@
 
 static char	*parse_words(char *str)
 {
+	int		flag;
 	char	*new;
 
+	flag = 0;
 	new = ft_strdup("");
-	if (*str == '\'')
-		str = convert_quotes(str + 1, &new);
-	else if (*str == '\"')
-		str = convert_dquotes(str + 1, &new);
-	else
-		str = convert_words(str, &new);
-	if (!str)
-		return (NULL);
 	while(*str)
 	{
-		if (*str == '\'' && *(str - 1) != '\\')
+		if (*str == '\'' && !flag)
 			str = convert_quotes(str + 1, &new);
-		else if (*str == '\"' && *(str - 1) != '\\')
+		else if (*str == '\"' && !flag)
 			str = convert_dquotes(str + 1, &new);
 		else
 			str = convert_words(str, &new);
 		if (!str)
 			return (NULL);
+		if (*str == '\\')
+			flag ^= ESC;
+		else
+			flag = 0;
 	}
 	return (new);
 }
