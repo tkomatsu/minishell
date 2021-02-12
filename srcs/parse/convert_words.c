1463c76a6f978d5633cfbef984bd4d3c2036a8a3
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-02-12 22:13:51 +0900

    fix(convert_words.c): fix bug when there are escape characters before quote, dquote
    - クォート前にエスケープが存在した場合の処理を修正
    
    fix partly #6

--- srcs/parse/convert_words.c
+++ srcs/parse/convert_words.c
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 02:05:04 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/02/12 02:24:58 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/02/12 21:53:41 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,20 @@ char	*convert_words(char *str, char **new)
 {
 	char	*tmp;
 	char	*sub;
+	int		flag;
 	int		len;
 
+	flag = 0;
 	len = 0;
-	while (!(str[len] == '\'' || str[len] == '\"' || str[len] == '\0'))
+	while (!(str[len] == '\'' || str[len] == '\"' || str[len] == '\0')
+			|| ((str[len] == '\'' || str[len] == '\"') && (flag & ESC)))
+	{
+		if (str[len] == '\\')
+			flag ^= ESC;
+		else
+			flag = 0;
 		len++;
+	}
 	if (!(sub = ft_substr(str, 0, len)))
 		return (NULL);
 	if (!(tmp = ft_strjoin(*new, sub)))
