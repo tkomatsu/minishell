38fd67bcea2d85703ffe2e705ff1d0185c1466f0
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-02-16 01:28:02 +0900

    fix(exit.c): add error handling when the number is more or less than max:min num of long long type
    - long long型の最大値以上、最小値以下の数字がexitの引数に渡された際のエラーハンドリング実装
    - 数値異常エラーのときの終了ステータスが255になるよう修正
    
    fix #43

--- srcs/execute/exit.c
+++ srcs/execute/exit.c
@@ -6,7 +6,7 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 23:23:13 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/02/14 03:07:38 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/02/16 01:09:09 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,62 @@
 
 static int	exit_error(char **args, int len, int num)
 {
-	g_status = EXIT_FAILURE;
 	errno = num;
 	if (num == 204)
 	{
+		g_status = 255;
 		ft_putstr_fd("minish: exit: ", 2);
 		ft_perror(args[len]);
 		return (0);
 	}
 	else
 	{
+		g_status = EXIT_FAILURE;
 		ft_putstr_fd("minish: ", 2);
 		ft_perror(args[0]);
 		return (1);
 	}
 }
 
-static int	is_numeric(char *str)
+static int	within_range(char *str, int is_negative)
 {
+	unsigned long long	num;
+	unsigned long long	max;
+	int					digit;
+
+	num = 0;
+	max = 9223372036854775807;
+	digit = 7;
+	if (is_negative)
+		digit = 8;
 	while (*str)
 	{
-		if (!ft_isdigit(*str))
+		if (num < max / 10 || (num == max / 10 && *str - '0' <= digit))
+			num = num * 10 + (*str - '0');
+		else
 			return (0);
 		str++;
 	}
 	return (1);
+}
+
+static int	is_numeric(char *str)
+{
+	int		is_negative;
+	char	*num;
+
+	is_negative = 0;
+	str = ft_strtrim(str, " ");
+	if (*str == '-' || *str == '+')
+	{
+		is_negative = (*str == '-' ? 1 : 0);
+		str++;
+	}
+	num = str;
+	while (*str)
+		if (!ft_isdigit(*(str++)))
+			return (0);
+	return (within_range(num, is_negative));
 }
 
 int			msh_exit(char **args)
