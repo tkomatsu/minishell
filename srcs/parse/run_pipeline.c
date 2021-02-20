ad511d0f5c6e83a27c17085de24b4c1f0eba4f82
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-02-20 21:58:45 +0900

    fix(run_pipeline): handle multiple pipeline
    #10

--- srcs/parse/run_pipeline.c
+++ srcs/parse/run_pipeline.c
@@ -6,61 +6,80 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 16:39:58 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/02/20 18:56:12 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/02/20 21:53:30 by tkomatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
-void	do_child(t_list *lst, int *newpipe)
+static void	child_proc(t_list *head, t_list *cur, int oldp[2], int newp[2])
 {
-	if (lst->next)
+	if (cur != head)
 	{
-		close(newpipe[0]);
-		dup2(newpipe[1], STDOUT);
-		close(newpipe[1]);
+		close(oldp[1]);
+		dup2(oldp[0], STDIN);
+		close(oldp[0]);
 	}
-	else
+	if (cur->next)
 	{
-		close(newpipe[1]);
-		dup2(newpipe[0], STDIN);
-		close(newpipe[0]);
+		close(newp[0]);
+		dup2(newp[1], STDOUT);
+		close(newp[1]);
 	}
-	g_status = run_cmd(lst->content, 1);
+	run_cmd(cur->content, 1);
 	exit(EXIT_SUCCESS);
 }
 
-int do_parent(t_list *lst, int *newpipe, int pid)
+static void	parent_proc(t_list *head, t_list *cur, int oldp[2], pid_t pid)
 {
 	int	status;
 
+	if (cur != head)
+	{
+		close(oldp[0]);
+		close(oldp[1]);
+	}
 	if (waitpid(pid, &status, WUNTRACED) < 0)
-		exit_perror("wait", 1);
-	if (lst->next)
-		close(newpipe[1]);
-	else
-		close(newpipe[0]);
-	return (status);
+		exit_perror("wait", EXIT_FAILURE);
 }
 
-int	run_pipeline(t_list *lst)
+static void	exec_pipeline(t_list *lst)
 {
 	pid_t	pid;
+	int		lastpipe[2];
 	int		newpipe[2];
-	int		status;
+	t_list	*head;
 
+	head = lst;
 	while (lst)
 	{
+		lastpipe[0] = newpipe[0];
+		lastpipe[1] = newpipe[1];
 		if (lst->next)
-			pipe(newpipe);
+			if (pipe(newpipe) < 0)
+				exit_perror("pipe", EXIT_FAILURE);
 		pid = fork();
 		if (pid < 0)
-			ft_perror("fork");
-		else if (pid == 0) // child
-			do_child(lst, newpipe);
-		else // parent
-			status = do_parent(lst, newpipe, pid);
+			exit_perror("fork", EXIT_FAILURE);
+		else if (pid == 0)
+			child_proc(head, lst, lastpipe, newpipe);
+		else
+			parent_proc(head, lst, lastpipe, pid);
 		lst = lst->next;
 	}
+}
+
+int			run_pipeline(t_list *lst)
+{
+	int	original_stdin;
+	int	original_stdout;
+
+	original_stdin = dup(STDIN);
+	original_stdout = dup(STDOUT);
+	exec_pipeline(lst);
+	dup2(original_stdin, STDIN);
+	close(original_stdin);
+	dup2(original_stdout, STDOUT);
+	close(original_stdout);
 	return (STAY_LOOP);
 }
