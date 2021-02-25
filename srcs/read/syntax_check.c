86c79ef915ebfaa9e4ce722d841b7b9cd9f40e23
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-02-25 11:24:24 +0900

    fix: fix syntax error
    メタ文字が重複していると、文字列が含まれていても文法エラーになってしまう問題を解決しました。
    
    fix #92

--- srcs/read/syntax_check.c
+++ srcs/read/syntax_check.c
@@ -6,7 +6,7 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 12:06:41 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/02/24 12:40:12 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/02/25 11:10:45 by tkomatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,17 @@ int	syntax_check(t_token *token)
 	type = "  |&;{}>><";
 	while (token)
 	{
-		if (token->type != WORD && token->next && token->type == token->next->type)
+		if (token->type != WORD && token->next && !*token->next->word
+				&& token->type == token->next->type)
 		{
 			ft_putstr_fd("minish: syntax error near unexpected token `", STDERR);
 			ft_putchar_fd(type[token->type - 1], STDERR);
 			ft_putstr_fd("\'\n", STDERR);
 			return (1);
 		}
-		if ((token->type == GREATER || token->type == GREATER2) && token->next &&
-			(token->next->type == GREATER || token->next->type == GREATER2))
+		if ((token->type == GREATER || token->type == GREATER2) && token->next
+				&& !*token->next->word
+				&& (token->next->type == GREATER || token->next->type == GREATER2))
 		{
 			ft_putstr_fd("minish: syntax error near unexpected token `", STDERR);
 			ft_putchar_fd(type[token->type - 1], STDERR);
