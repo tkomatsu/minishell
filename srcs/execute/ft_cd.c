573924fcb41f813a00f210f141757bde5d3d5a8b
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-03-01 13:33:33 +0900

    fix: cd synbolic link
    シンボリックリンクに対応しました。
    #109

--- srcs/execute/ft_cd.c
+++ srcs/execute/ft_cd.c
@@ -6,38 +6,64 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 15:05:15 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/02/28 22:51:00 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2021/03/01 13:29:51 by tkomatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute.h"
 
-static int	change_dir(char *dir)
+static void	set_cwd(char *dir)
 {
 	char	*path;
 	char	*tmp;
+
+	ft_setenv("OLDPWD", ft_getenv("PWD"), 1);
+	if (ft_strcmp(dir, ".."))
+	{
+		tmp = ft_strjoin(ft_getenv("OLDPWD"), "/");
+		path = ft_strjoin(tmp, dir);
+		free(tmp);
+	}
+	else
+	{
+		path = ft_getenv("PWD");
+		tmp = ft_strrchr(path, '/');
+		*tmp = '\0';
+	}
+	ft_setenv("PWD", path, 1);
+}
+
+/*
+static int	change_dir(char *dir)
+{
 	int		has_pwd;
 
 	has_pwd = 0;
+	if (ft_getenv("PWD"))
+		has_pwd = 1;
+	return (EXIT_SUCCESS);
+}
+*/
+
+static int	change_dir(char *dir)
+{
+	char	*path;
+
 	if (chdir(dir) < 0)
 	{
 		ft_putstr_fd("minish: cd: ", STDERR);
 		ft_perror(dir);
 		return (EXIT_FAILURE);
 	}
-	if (ft_getenv("PWD"))
-		has_pwd = 1;
-	ft_setenv("OLDPWD", ft_getenv("PWD"), 1);
-	if (has_pwd)
+	if (!ft_getenv("PWD"))
 	{
-		tmp = ft_strjoin(ft_getenv("OLDPWD"), "/");
-		path = ft_strjoin(tmp, dir);
-		free(tmp);
+		if (!(path = get_working_dir("cd")))
+			path = ft_strdup(dir);
+		ft_setenv("PWD", path, 1);
+		free(path);
+		return (EXIT_SUCCESS);
 	}
-	else if (!(path = get_working_dir("cd")))
-		path = ft_strdup(dir);
-	ft_setenv("PWD", path, 1);
-	free(path);
+	set_cwd(dir);
 	return (EXIT_SUCCESS);
 }
 
@@ -45,7 +71,7 @@ int			ft_cd(char **args)
 {
 	char	*dir;
 
-	if (!args[1] || !ft_strcmp(args[1], "~"))
+	if (!args[1])
 	{
 		if (!(dir = ft_getenv("HOME")))
 		{
